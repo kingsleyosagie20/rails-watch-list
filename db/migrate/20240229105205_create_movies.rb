72457dfe0792db64f6df7a960477dc42e6e52f88
Kingsley Osagie <osagiekingsley20@icloud.com>
2024-03-04 11:58:35 +0200

    rails watch list1

--- db/migrate/20240229105205_create_movies.rb
+++ db/migrate/20240229105205_create_movies.rb
@@ -1,10 +1,10 @@
-class CreateMovies < ActiveRecord::Migration[7.1]
+class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
-      t.string :title,
-      t.string :overview,
-      t.string :poster_url,
-      t.interger :rating
+      t.string :title
+      t.text :overview
+      t.string :poster_url
+      t.float :rating
 
       t.timestamps
     end
--- db/migrate/20240229105952_create_bookmarks.rb
+++ db/migrate/20240229105952_create_bookmarks.rb
@@ -1,9 +1,9 @@
-class CreateBookmarks < ActiveRecord::Migration[7.1]
+class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
-      t.string :comment,
-      t.string :movie_id,
-      t.string :list_id
+      t.text :comment
+      t.references :movie, null: false, foreign_key: true
+      t.references :list, null: false, foreign_key: true
 
       t.timestamps
     end
